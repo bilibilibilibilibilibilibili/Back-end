b105ca8394fac148aba0e5c98157c4cf57dd3bbc
Eason_R <rys582132403@163.com>
2023-03-22 14:05:07 +0800

    fix import bug

--- src/main/java/com/example/forumBackEnd/pojo/Post.java
+++ src/main/java/com/example/forumBackEnd/pojo/Post.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.junit.Test;
 
 import java.io.IOException;
 import java.util.ArrayList;
